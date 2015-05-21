b36462f009bcfb19b6fdb9dd4a8967507159ee9e
Dave Coyle <hello@coyled.com>
2015-05-21 08:43:11 -0700

    add urlencode alias

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,2 +1,3 @@
 alias history="history -t '%Y-%m-%dT%H:%M:%S %Z'"
 alias screen="export TERM=screen; screen"
+alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
