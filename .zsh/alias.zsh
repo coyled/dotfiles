ce2b53d8bede9f7f04b75414738150d438fdf0a4
Dave Coyle <hello@coyled.com>
2016-09-03 23:55:31 -0700

    add ls/sl/whois aliases

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,3 +1,6 @@
 alias history="history -t '%Y-%m-%dT%H:%M:%S %Z'"
+alias ls="ls --color"
 alias screen="export TERM=screen; screen"
+alias sl="readlink -e"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias whois="whois -H"
