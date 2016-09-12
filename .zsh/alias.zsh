6f0fbd75a9471636fdf13dac114ffc0a231ce2f4
Dave Coyle <hello@coyled.com>
2016-09-11 19:48:40 -0700

    alias vi to vim

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -3,4 +3,5 @@ alias ls="ls --color"
 alias screen="export TERM=screen; screen"
 alias sl="readlink -e"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
+alias vi="vim"
 alias whois="whois -H"
