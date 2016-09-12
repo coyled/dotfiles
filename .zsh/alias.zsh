8f60c531b83ff68de47dc36aeb3cc5dd6c7c2eff
Dave Coyle <hello@coyled.com>
2016-09-11 20:19:30 -0700

    don't alias ls on os x

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,7 +1,10 @@
 alias history="history -t '%Y-%m-%dT%H:%M:%S %Z'"
-alias ls="ls --color"
 alias screen="export TERM=screen; screen"
 alias sl="readlink -e"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias vi="vim"
 alias whois="whois -H"
+
+if [ `uname -s` != 'Darwin' ]; then
+    alias ls="ls --color"
+fi
