a7737e63a8388648f90ac78d0b2d2ca45e279134
Dave Coyle <hello@coyled.com>
2016-10-04 16:45:15 -0700

    switch to some more sensible term coloring
    i had picked up some $TERM/screen coloring settings at some point over
    the years but they don't make sense so rm'ing them.

--- .zsh/alias.zsh
+++ .zsh/alias.zsh
@@ -1,6 +1,5 @@
 alias history="history -t '%Y-%m-%dT%H:%M:%S %Z'"
 alias pwgen='apg -a 1 -m 32 -x 32 -M SNCL -n 1 -c /dev/random'
-alias screen="export TERM=screen; screen"
 alias sl="readlink -e"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias vi="vim"
