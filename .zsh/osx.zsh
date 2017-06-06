0ed552f3dc24977ec2c0a9f9356749e4cfcceea8
Dave Coyle <hello@coyled.com>
2017-06-06 11:16:21 -0700

    .zsh/osx.zsh: add brew command for gcloud

--- .zsh/osx.zsh
+++ .zsh/osx.zsh
@@ -3,6 +3,9 @@ if [[ $(uname -s) == 'Darwin' ]]; then
     # don't swallow ctrl-o so i can use it in mutt...
     stty discard undef
 
+    #
+    # brew cask install google-cloud-sdk
+    #
     source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc'
     source '/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc'
 
