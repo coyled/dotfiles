97f3c5561e6fd4180a1b74338a55703ea2adebec
Dave Coyle <hello@coyled.com>
2018-04-17 14:37:30 -0700

    don't alias whois on macos

--- .zsh/80-alias.zsh
+++ .zsh/80-alias.zsh
@@ -4,13 +4,14 @@ alias pwgen='apg -a 1 -m 32 -x 32 -M SNCL -n 1 -c /dev/random'
 alias sl="readlink -e"
 alias urlencode='python -c "import sys, urllib as ul; print ul.quote_plus(sys.argv[1])"'
 alias vi="vim"
-alias whois="whois -H"
 
 if [ `uname -s` != 'Darwin' ]; then
     alias ls="ls --color"
 
     # ls files that are open but have been deleted...
     alias lsof-unlinked="lsof -nP +L1"
+
+    alias whois="whois -H"
 fi
 
 alias restart-gpg-agent="gpg-connect-agent reloadagent /bye"
