8c2e1e46b127671f41dd35b7c872121873f11b07
Dave Coyle <hello@coyled.com>
2019-07-12 07:30:46 -0700

    on ubuntu add `/snap/bin` to `$PATH`
    fixes #4

--- .zsh/10-path.zsh
+++ .zsh/10-path.zsh
@@ -1,2 +1,11 @@
 export PATH=$HOME/bin:$HOME/sbin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PATH=$PATH:~/.rbenv/bin
+
+if [[ -f /etc/lsb-release ]]; then
+    . /etc/lsb-release
+    if [[ $DISTRIB_ID == 'Ubuntu' ]]; then
+        if [[ -d /snap/bin ]]; then
+            export PATH=$PATH:/snap/bin
+        fi
+    fi
+fi
