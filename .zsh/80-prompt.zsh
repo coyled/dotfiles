7a29e678d14c168eac09f310dc8220389d9f5744
Dave Coyle <hello@coyled.com>
2019-07-13 16:32:09 -0700

    zsh-git-prompt: work with either python2 or python3
    upstream hardcoded `python`, which doesn't exist out-of-the-box on
    Ubuntu >= 18.04.  this change detects which is available.
    
    fixes #5

--- .zsh/80-prompt.zsh
+++ .zsh/80-prompt.zsh
@@ -1,7 +1,19 @@
 autoload -U colors
 colors
 
-. ~/.zsh-git-prompt/zshrc.sh
+#
+# ubuntu >= 18.04 doen't ship with python2 but we can't assume python3
+# is installed everywhere so test which exists...
+#
+if [[ -e $(which python3) ]]; then
+    GIT_PROMPT_EXECUTABLE="python3"
+    . ~/.zsh-git-prompt/zshrc.sh
+elif [[ -e $(which python) ]]; then
+    GIT_PROMPT_EXECUTABLE="python"
+    . ~/.zsh-git-prompt/zshrc.sh
+else
+    echo "oops.  can't find python.  no git prompt for you."
+fi
 
 PROMPT=' %n@%m: %~ $(git_super_status) $fg[magenta]--->%{$reset_color%}
  %# '
