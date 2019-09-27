60041e21c741b038905c0e829213df9e6ad5e1aa
Dave Coyle <hello@coyled.com>
2019-09-27 13:35:34 -0700

    add curl-timing alias

--- .zsh/80-alias.zsh
+++ .zsh/80-alias.zsh
@@ -1,3 +1,22 @@
+alias curl-timing='curl -w \
+"{
+    \"local_ip\": \"%{local_ip}\",
+    \"remote_ip\": \"%{remote_ip}\",
+    \"effective_url\": \"%{url_effective}\",
+    \"http_code\": \"%{http_code}\",
+    \"size_download\": \"%{size_download}\",
+    \"size_request\": \"%{size_request}\",
+    \"times\": {
+        \"redirect\": \"%{time_redirect}\",
+        \"namelookup\": \"%{time_namelookup}\",
+        \"connect\": \"%{time_connect}\",
+        \"appconnect\": \"%{time_appconnect}\",
+        \"pretransfer\": \"%{time_pretransfer}\",
+        \"starttransfer\": \"%{time_starttransfer}\",
+        \"total\": \"%{time_total}\"
+    }
+}\n" \
+    -o /dev/null -s'
 alias history="history -t '%Y-%m-%dT%H:%M:%S %Z'"
 alias lc='tr "[A-Z]" "[a-z]"'
 alias pwgen='apg -a 1 -m 32 -x 32 -M SNCL -n 1 -c /dev/random'
