c894bedd4f2b417fd63209fbd4eab5733faddda3
Dave Coyle <hello@coyled.com>
2025-06-07 07:18:07 +0200

    add ~/.go/bin to $PATH

--- .zsh/10-path.zsh
+++ .zsh/10-path.zsh
@@ -1,5 +1,6 @@
 export PATH=$HOME/bin:$HOME/sbin:$HOME/.local/bin:/usr/local/bin:/usr/local/sbin:$PATH
 export PATH=$PATH:~/.rbenv/bin
+export PATH=$PATH:~/.go/bin
 
 if [[ -f /etc/lsb-release ]]; then
     . /etc/lsb-release
