75406e08974825eedcc620992d088ccda534ffba
Dave Coyle <hello@coyled.com>
2025-07-22 06:36:54 +0200

    zsh: update PAGER, TMOUT

--- .zsh/00-env.zsh
+++ .zsh/00-env.zsh
@@ -1,8 +1,8 @@
-export PAGER=less
+export PAGER='less -XR'
 export EDITOR=vim
 export HISTSIZE=1000
 export SAVEHIST=5
-export TMOUT=86400
+export TMOUT=604800     # 7 days
 export GOPATH=~/.go
 
 export HOSTNAME=$(hostname -s)
